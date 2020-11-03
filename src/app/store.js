b89ea4af0cad4dda32e9f2e082ad493aaf25814f
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-11-03 18:47:54 -0500

    add enhancers and middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,12 @@
+// import { compose } from "redux";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-
+import logger from "redux-logger";
+import persistState from "redux-localstorage";
+// import { reduxBatch } from '@manaflair/redux-batch'
 import carReducer from "../features/car/carSlice";
 import featuresReducer from "../features/addFeatures/featuresSlice";
+
+// const enhancer = compose()
 
 const rootReducer = combineReducers({
 	car: carReducer,
@@ -10,4 +15,6 @@ const rootReducer = combineReducers({
 
 export default configureStore({
 	reducer: rootReducer,
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+	enhancers: [persistState(),]
 })
